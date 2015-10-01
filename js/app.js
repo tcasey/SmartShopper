0ea8b54c4538464bbdf09d69d2e839d854ef19f0
tcasey <trevort.casey@gmail.com>
2015-10-01 10:00:27 -0600

    minor changes

--- js/app.js
+++ js/app.js
@@ -3,25 +3,26 @@ $(document).ready(function() {
         var input = ()*/
 
     $('.emptyCart-button').click(function() {
-        //alert('Test');
+        console.log( $( this ).text() );
         //$('.items').empty(); //empty's all children
         $('li').remove(); //removes all classes that match li
     });
 
     $('.submit-button').click(function() {
-        //alert('Test');
+           console.log( $( this ).text() );
         var input = $('.enter-item').val();//current value that user inputed in text box
         $('.items').prepend('<li>' + input + '<span class="icons"><a href="#" class="unchecked-box"><i class="fa fa-square-o"></i></a><a href="#" class="delete-circle"><i class="fa fa-minus-circle"></i></a></span></li>');
         $('.enter-item').val('');//setting input to blank
     });
 
     $('.items').on('click', '.delete-circle', function() { 
-        //alert('test');
+        console.log( $( this ).text() );
         $(this).closest('li').remove(); //find 'this' object. find closest list element
     });
 
     $('.items').on('click', '.unchecked-box', function() {
-        //alert('test');
-
-    });
+        console.log( $( this ).text() );
+   });
+     // define what happens when we click the "Add Item" link
+    
 })
